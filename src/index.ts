da103c61fc1bf4f6a7da38bdfde8b0a4b5689662
Steve Smodish <smodish@hotmail.com>
2022-05-17 16:54:43 -0400

    sort number array

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-console.log(123)
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const log = (something: string) => {
-  console.log(something)
+  sort(): void {
+    const { length } = this.collection
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j]
+          this.collection[j] = this.collection[j + 1]
+          this.collection[j + 1] = temp
+        }
+      }
+    }
+  }
 }
 
-log('hey hey hey')
+const sorter = new Sorter([10, 3, -5, 0])
+sorter.sort()
+console.log(sorter.collection)
