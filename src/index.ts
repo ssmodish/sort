de079e00bb7cbf10b8a086f3e59a167c4b8393d1
Steve Smodish <smodish@hotmail.com>
2022-05-17 17:47:50 -0400

    intermediate refactor for interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
 
-  sort(): void {
-    const { length } = this.collection
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // Handles an array of numbers
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const temp = this.collection[j]
-            this.collection[j] = this.collection[j + 1]
-            this.collection[j + 1] = temp
-          }
-        }
-
-        // Handles a string
-        if (typeof this.collection === 'string') {
-          console.log(this.collection)
-        }
-      }
-    }
-  }
-}
-
-const numSorter = new Sorter([10, 3, -5, 0])
-numSorter.sort()
-console.log(numSorter.collection)
-
-const stringSorter = new Sorter('XaxBSE')
-stringSorter.sort()
-console.log(stringSorter)
+const numbersCollection = new NumbersCollection([10, 88, 3, -5, 0])
+const sorter = new Sorter(numbersCollection)
+sorter.sort()
+console.log(sorter.collection.data)
